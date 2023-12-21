5347482b3325c01483067895cddfde7f030981ba
Jembo737 <74143229+Jembo737@users.noreply.github.com>
2023-12-21 15:26:18 +0400

    "Init commit"

---
+++ GHFollowers737/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers737
+//
+//  Created by Dzhemal on 21.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
