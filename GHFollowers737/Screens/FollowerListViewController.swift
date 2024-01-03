3ad7eb7448b1bad3170c304a240ed6309437173e
User <user@YJQM1VX3K6.local>
2024-01-03 14:46:32 +0400

    Made NetworkManager refactoring on <Result> type in completed escaping closure

--- GHFollowers737/Screens/FollowerListViewController.swift
+++ GHFollowers737/Screens/FollowerListViewController.swift
@@ -16,13 +16,14 @@ class FollowerListViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         view.backgroundColor = .systemBackground
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMesage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad things happend", message: errorMesage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad things happend", message: error.rawValue, buttonTitle: "Ok")
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
     }
     
--- GHFollowers737/Utilities/ErrorMessage.swift
+++ GHFollowers737/Utilities/ErrorMessage.swift
@@ -5,7 +5,7 @@
 //  Created by User on 28.12.23.
 //
 
-enum ErrorMessage: String {
+enum GFError: String, Error {
     case invalidUsername = "This user name created an invalid request. Please try again"
     case noInternet = "Unable to complete your request. Please check your internet connection"
     case serverIsDown = "Invalid response from the server. Please try again."
