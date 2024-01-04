a899d193ae9be6e2072ab0b27266c37fd69426ac
User <user@YJQM1VX3K6.local>
2024-01-04 15:29:09 +0400

    Added custom CollectionViewController and custom Cell file

--- GHFollowers737/Screens/FollowerListViewController.swift
+++ GHFollowers737/Screens/FollowerListViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 class FollowerListViewController: UIViewController {
     
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .systemBackground
-        
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //        navigationController?.navigationBar.isHidden = false  have a bug where navigation controller dissapears
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
@@ -27,9 +37,15 @@ class FollowerListViewController: UIViewController {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        //        navigationController?.navigationBar.isHidden = false  have a bug where navigation controller dissapears
-        navigationController?.setNavigationBarHidden(false, animated: true)
+    func configureViewController() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
 }
