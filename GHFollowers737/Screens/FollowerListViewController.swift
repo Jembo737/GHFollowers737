7178bd6561169d0221604fbba7f9db5c7e25d162
User <user@YJQM1VX3K6.local>
2024-01-08 14:42:46 +0400

    Added CollectionViewFlowLayout

--- GHFollowers737/Screens/FollowerListViewController.swift
+++ GHFollowers737/Screens/FollowerListViewController.swift
@@ -25,9 +25,33 @@ class FollowerListViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
+    func configureViewController() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColomnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColomnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
     func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            
             switch result {
             case .success(let followers):
                 print(followers)
@@ -35,17 +59,5 @@ class FollowerListViewController: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Bad things happend", message: error.rawValue, buttonTitle: "Ok")
             }
         }
-    }
-    
-    func configureViewController() {
-        navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .systemBackground
-    }
-    
-    func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
-        view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemPink
-        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
 }
